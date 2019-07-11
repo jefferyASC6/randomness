ecced989d9fd112307efd983a01a1c00cfa1f602
ndhbASC6 <nicolas.hughesbarrow0@gmail.com>
2019-07-11 14:09:12 -0400

    main

---
+++ randomness.js
@@ -0,0 +1,46 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+function randomNumber() {
+    console.log(Math.random() * 21)
+}
+randomNumber();
+function randomLetter() {
+    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+}
+randomLetter();
+function randomFood() {
+    console.log(foods[Math.floor(Math.random() * foods.length)]);
+}
+randomFood();
+function randomStudent() {
+    console.log(students[Math.floor(Math.random() * students.length)]);
+}
+randomStudent();
+function randomGroup() {
+    for(let i = 0; i < 3; i++) {
+        randomStudent();
+    }
+}
+randomGroup();
